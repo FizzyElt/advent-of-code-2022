aca81ed4f4dd6f5c25cdbcbad9980c6da8a356e5
FizzyElt <fizzyelt8786@gmail.com>
2022-12-02 20:50:14 +0800

    solve day 2 part 2

--- day-2/index.ts
+++ day-2/index.ts
@@ -6,6 +6,7 @@ import { flow, pipe } from 'fp-ts/function';
 import { cond, equals, always, where, split, trim, sum } from 'ramda';
 
 type RPS = 'Rock' | 'Paper' | 'Scissors';
+type GameStatus = 'Win' | 'Lose' | 'Draw';
 
 const getDataContent = (filePath: string): TE.TaskEither<Error, string> =>
   TE.tryCatch(
@@ -13,28 +14,46 @@ const getDataContent = (filePath: string): TE.TaskEither<Error, string> =>
     (err) => new Error(`${err}`)
   );
 
-const formatRPS = (value: string): 'Rock' | 'Paper' | 'Scissors' => {
+const formatRPS = (value: string): RPS => {
   if (equals(value, 'A') || equals(value, 'X')) return 'Rock';
   if (equals(value, 'B') || equals(value, 'Y')) return 'Paper';
   if (equals(value, 'C') || equals(value, 'Z')) return 'Scissors';
   throw Error('not match string');
 };
 
-const sliceContent = flow(
-  trim,
-  split('\n'),
-  A.map(split(' ')),
-  A.map(([left, right]) => [formatRPS(left), formatRPS(right)])
-);
+const formatGameResult = (value: string): GameStatus => {
+  if (equals(value, 'X')) return 'Lose';
+  if (equals(value, 'Y')) return 'Draw';
+  if (equals(value, 'Z')) return 'Win';
+  throw Error('not match string');
+};
+
+const sliceContent = flow(trim, split('\n'), A.map(split(' ')));
 
 const isRock = equals('Rock');
 const isPaper = equals('Paper');
 const isScissors = equals('Scissors');
 
+const isWin = equals('Win');
+const isLose = equals('Lose');
+const isDraw = equals('Draw');
+
+const getGameScore = cond<[GameStatus], number>([
+  [isWin, always(6)],
+  [isLose, always(0)],
+  [isDraw, always(3)],
+]);
+
+const getTypeScore = cond<[RPS], number>([
+  [isRock, always(1)],
+  [isPaper, always(2)],
+  [isScissors, always(3)],
+]);
+
 const getRightPlayerScore = ([left, right]: [RPS, RPS]): number => {
-  const lose = always(0);
-  const draw = always(3);
-  const win = always(6);
+  const lose = always(getGameScore('Win'));
+  const draw = always(getGameScore('Draw'));
+  const win = always(getGameScore('Lose'));
 
   return cond([
     [where({ left: isRock, right: isRock }), draw],
@@ -51,21 +70,56 @@ const getRightPlayerScore = ([left, right]: [RPS, RPS]): number => {
   ])({ left, right });
 };
 
-const getTypeScore = cond<[RPS], number>([
-  [isRock, always(1)],
-  [isPaper, always(2)],
-  [isScissors, always(3)],
-]);
+const getPlayerRPS = (rps: RPS, res: GameStatus): RPS => {
+  return cond<[{ rps: RPS; res: GameStatus }], RPS>([
+    [where({ rps: isRock, res: isWin }), always('Paper')],
+    [where({ rps: isRock, res: isLose }), always('Scissors')],
+    [where({ rps: isRock, res: isDraw }), always('Rock')],
+
+    [where({ rps: isPaper, res: isWin }), always('Scissors')],
+    [where({ rps: isPaper, res: isLose }), always('Rock')],
+    [where({ rps: isPaper, res: isDraw }), always('Paper')],
+
+    [where({ rps: isScissors, res: isWin }), always('Rock')],
+    [where({ rps: isScissors, res: isLose }), always('Paper')],
+    [where({ rps: isScissors, res: isDraw }), always('Scissors')],
+  ])({ rps, res });
+};
 
 const part1Flow = flow(
   sliceContent,
+  A.map(([left, right]) => [formatRPS(left), formatRPS(right)]),
   A.map(([left, right]) => getRightPlayerScore([left, right]) + getTypeScore(right)),
   sum
 );
 
+const part2Flow = flow(
+  sliceContent,
+  A.map<Array<string>, [RPS, GameStatus]>(([left, right]) => [
+    formatRPS(left),
+    formatGameResult(right),
+  ]),
+  A.map(
+    ([opponentRPS, result]) =>
+      getGameScore(result) + getTypeScore(getPlayerRPS(opponentRPS, result))
+  ),
+  sum
+);
+
+// part 1
 pipe(
   getDataContent('./day-2/data.txt'),
   TE.map(part1Flow),
+  TE.match(
+    (err) => console.log('you got some error', err),
+    (res) => console.log('answer is', res)
+  )
+)();
+
+// part 2
+pipe(
+  getDataContent('./day-2/data.txt'),
+  TE.map(part2Flow),
   TE.match(
     (err) => console.log('you got some error', err),
     (res) => console.log('answer is', res)
