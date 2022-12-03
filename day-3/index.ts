0849b4fd9fd01d9e40fbbeff11153036c5a22cb1
FizzyElt <fizzyelt8786@gmail.com>
2022-12-03 16:11:31 +0800

    solve day3 part2 problem

--- day-3/index.ts
+++ day-3/index.ts
@@ -4,16 +4,9 @@ import { flow, pipe } from 'fp-ts/function';
 
 import { getDataContent } from '../utils/readFileTask';
 
-import { reduce, split, trim, sum, splitAt, intersection, uniq, flatten } from 'ramda';
+import { reduce, split, trim, sum, splitAt, intersection, uniq, equals } from 'ramda';
 
-const sliceContent = flow(
-  trim,
-  split('\n'),
-  A.map((str) => splitAt(Math.floor(str.length / 2), str))
-);
-
-const findDuplicateChars = ([left, right]: [string, string]): Array<string> =>
-  intersection(uniq([...left]), uniq([...right]));
+const sliceContent = flow(trim, split('\n'));
 
 const getPriorityCode = (char: string) => {
   const charCode = char.charCodeAt(0);
@@ -21,11 +14,38 @@ const getPriorityCode = (char: string) => {
   return charCode - 'A'.charCodeAt(0) + 27;
 };
 
-const part1Flow = flow(sliceContent, A.chain(findDuplicateChars), A.map(getPriorityCode), sum);
+const findTheCommonChars = A.reduce<string, Array<string>>([], (acc, str) =>
+  equals(acc.length, 0) ? uniq([...str]) : intersection(acc, uniq([...str]))
+);
+
+const part1Flow = flow(
+  sliceContent,
+  A.map((str) => splitAt(Math.floor(str.length / 2), str)),
+  A.chain(findTheCommonChars),
+  A.map(getPriorityCode),
+  sum
+);
+
+const part2Flow = flow(
+  sliceContent,
+  A.chunksOf(3),
+  A.chain(findTheCommonChars),
+  A.map(getPriorityCode),
+  sum
+);
 
 pipe(
   getDataContent('./day-3/data.txt'),
   TE.map(part1Flow),
+  TE.match(
+    (err) => console.log('you got some error', err),
+    (res) => console.log('answer is', res)
+  )
+)();
+
+pipe(
+  getDataContent('./day-3/data.txt'),
+  TE.map(part2Flow),
   TE.match(
     (err) => console.log('you got some error', err),
     (res) => console.log('answer is', res)
