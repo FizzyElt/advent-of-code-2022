5488c7a36b43dd0e6c0593c6808bcb5448ec5536
FizzyElt <fizzyelt8786@gmail.com>
2022-12-06 15:59:13 +0800

    remove not use function

--- day-6/index.ts
+++ day-6/index.ts
@@ -1,11 +1,9 @@
 import * as TE from 'fp-ts/TaskEither';
 import { pipe, flow } from 'fp-ts/function';
 
-import { uniq, slice, equals, trim, tap } from 'ramda';
+import { uniq, slice, equals, trim } from 'ramda';
 
 import { getDataContent } from '../utils/readFileTask';
-
-const sliceContent = flow(trim);
 
 const findFirstMarker = (checkNumOfChar: number) => (str: string) => {
   return [...str].findIndex((_c, index, chars) => {
@@ -18,9 +16,9 @@ const findFirstMarker = (checkNumOfChar: number) => (str: string) => {
   });
 };
 
-const part1Flow = flow(sliceContent, findFirstMarker(4));
+const part1Flow = flow(trim, findFirstMarker(4));
 
-const part2Flow = flow(sliceContent, findFirstMarker(14));
+const part2Flow = flow(trim, findFirstMarker(14));
 
 pipe(
   getDataContent('./day-6/data.txt'),
